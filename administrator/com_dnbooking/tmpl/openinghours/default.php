dee470ee3022803ac2b7467440d9b5ccc2fc219f
WhykikiVMM <info@whykiki.de>
2024-03-21 13:02:40 +0100

    Kalender angepasst / vorbereitet

--- administrator/com_dnbooking/tmpl/openinghours/default.php
+++ administrator/com_dnbooking/tmpl/openinghours/default.php
@@ -18,6 +18,7 @@ use Joomla\CMS\Layout\LayoutHelper;
 /** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
 $wa = $this->document->getWebAssetManager();
 $wa->useScript('com_dnbooking.calendar');
+$wa->useStyle('com_dnbooking.admin-calendar');
 
 $user      = Factory::getApplication()->getIdentity();
 $userId    = $user->get('id');
@@ -26,37 +27,18 @@ $userId    = $user->get('id');
 <form action="<?php echo Route::_('index.php?option=com_dnbooking&view=openinghours'); ?>" method="post" name="adminForm" id="adminForm">
     <div class="row">
         <div class="col-md-12">
-            <div id="calendar-container" class="calendar-container">
-                <div id="selectDate">
-                    <div>
-                        <input type="number" name="year" id="yearSelect" class="form-control" step="1" min="<?php echo date("Y"); ?>" value="<?php echo date("Y"); ?>"/>
-                    </div>
-                    <div >
-                        <select name="month" id="monthSelect" class="form-control">
-                            <option value="0"><?php echo Text::_('COM_DNBOOKING_CALENDAR_SELECT_MONTH')?></option>
-                            <option value="1"><?php echo Text::_('COM_DNBOOKING_CALENDAR_JANUARY')?></option>
-                            <option value="2"><?php echo Text::_('COM_DNBOOKING_CALENDAR_FEBRUARY')?></option>
-                            <option value="3"><?php echo Text::_('COM_DNBOOKING_CALENDAR_MARCH')?></option>
-                            <option value="4"><?php echo Text::_('COM_DNBOOKING_CALENDAR_APRIL')?></option>
-                            <option value="5"><?php echo Text::_('COM_DNBOOKING_CALENDAR_MAY')?></option>
-                            <option value="6"><?php echo Text::_('COM_DNBOOKING_CALENDAR_JUNE')?></option>
-                            <option value="7"><?php echo Text::_('COM_DNBOOKING_CALENDAR_JULY')?></option>
-                            <option value="8"><?php echo Text::_('COM_DNBOOKING_CALENDAR_AUGUST')?></option>
-                            <option value="9"><?php echo Text::_('COM_DNBOOKING_CALENDAR_SEPTEMBER')?></option>
-                            <option value="10"><?php echo Text::_('COM_DNBOOKING_CALENDAR_OCTOBER')?></option>
-                            <option value="11"><?php echo Text::_('COM_DNBOOKING_CALENDAR_NOVEMBER')?></option>
-                            <option value="12"><?php echo Text::_('COM_DNBOOKING_CALENDAR_DECEMBER')?></option>
-                        </select>
+                <div class="jumbotron">
+                    <h1 class="text-center"><a id="left" href="#"><i class="fa fa-chevron-left"> </i></a><span>&nbsp;</span><span id="month"> </span><span>&nbsp;</span><span id="year"> </span><span>&nbsp;</span><a id="right" href="#"><i class="fa fa-chevron-right"> </i></a></h1>
+                </div>
+                <div class="row">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        <table class="table"></table>
                     </div>
                 </div>
-                <div id="calendar">
-
-                </div>
-
-                <?php echo HTMLHelper::_('form.token'); ?>
-                <input type="hidden" name="task" value="" />
-                <input type="hidden" name="boxchecked" value="0" />
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
+    <?php echo HTMLHelper::_('form.token'); ?>
+    <input type="hidden" name="task" value="" />
+    <input type="hidden" name="boxchecked" value="0" />
 </form>
--- media/com_dnbooking/js/calendar.js
+++ media/com_dnbooking/js/calendar.js
@@ -1,63 +1,115 @@
-/**
- * @copyright  (C) Add your copyright here
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-// Function to generate calendar
-function generateCalendar(year, month) {
-    // Days of the week
-    const days = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
-
-    // First day of the month
-    let firstDay = new Date(year, month).getDay();
-    firstDay = (firstDay === 0) ? 6 : firstDay - 1; // Adjust for Monday start
-
-    // Last day of the month
-    let lastDay = new Date(year, month + 1, 0).getDate();
-
-    // Generate calendar header
-    let calendar = '<table class="table"><thead><tr>' + days.map(day => `<th>${day}</th>`).join('') + '</tr></thead><tbody><tr>';
-
-    // Add empty cells for days of the previous month
-    for (let i = 0; i < firstDay; i++) {
-        calendar += '<td></td>';
-    }
-
-    // Add cells for each day of the month
-    for (let i = 1; i <= lastDay; i++) {
-        if ((i + firstDay) % 7 === 0) {
-            calendar += `<td>${i}</td></tr><tr>`;
-        } else {
-            calendar += `<td>${i}</td>`;
-        }
-    }
-
-    // Close the last row
-    calendar += '</tr></tbody></table>';
-
-    // Insert the calendar into the div
-    document.getElementById('calendar').innerHTML = calendar;
-}
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
-    let date = new Date();
-    const yearSelect = document.getElementById('yearSelect');
-    const monthSelect = document.getElementById('monthSelect');
-    generateCalendar(date.getFullYear(), date.getMonth());
-    yearSelect.addEventListener('change', function() {
-        generateCalendar(this.value, monthSelect.value);
+    var currentDate = new Date();
+
+    var dayIds = {
+        '2024-03-01': 1,
+        '2024-03-15': 2,
+        '2024-03-20': 3,
+        // Weitere Daten
+    };
+
+    function formatDate(d) {
+        return d.getFullYear() + '-' + String(d.getMonth() + 1).padStart(2, '0') + '-' + String(d.getDate()).padStart(2, '0');
+    }
+
+    function monthDays(d) {
+        return new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();
+    }
+
+    function sendTaskRequest(task, dayId, date) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/path/to/joomla/task/' + task); // Ändern Sie dies in Ihre Joomla-Task-URL
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                alert('Task erfolgreich: ' + task + ' für ' + date);
+                // Hier könnten Sie die Kalenderansicht aktualisieren oder andere Aktionen durchführen
+            } else {
+                alert('Fehler beim Ausführen des Tasks: ' + task);
+            }
+        };
+        xhr.send('id=' + dayId + '&date=' + date); // Senden Sie die notwendigen Daten
+    }
+
+    function generateCalendar(d) {
+        var lang = document.documentElement.lang || 'en-gb'; // Standardmäßig auf Englisch (Großbritannien), falls kein Lang-Attribut gesetzt ist
+        const translations = {
+            'en-gb': {
+                weekDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+                months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+            },
+            'de-de': {
+                weekDays: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+                months: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
+            }
+            // Fügen Sie hier weitere Sprachen hinzu
+        };
+
+        var details = {
+            totalDays: monthDays(d),
+            weekDays: translations[lang].weekDays,
+            months: translations[lang].months,
+        };
+
+        var start = new Date(d.getFullYear(), d.getMonth()).getDay();
+        var cal = [];
+        var day = 1;
+        for (var i = 0; i <= 6; i++) {
+            cal.push(['<tr>']);
+            for (var j = 0; j < 7; j++) {
+                var date = new Date(d.getFullYear(), d.getMonth(), day - start + 1);
+                var dateString = formatDate(date);
+                var dayId = dayIds[dateString]; // Nehmen Sie an, dass dayIds ein externes Objekt ist
+                var classes = 'day' + (dayId ? ' closed' : '');
+                var icons = dayId ? '<div class="icons"><span class="icon-edit editCalendar" data-id="' + dayId + '" aria-hidden="true"></span><span class="icon-trash trashCalendar" data-id="' + dayId + '" aria-hidden="true"><div></div>' : '<div class="icons"><i class="add-icon addCalendar">➕</i></div>';
+                if (i === 0) {
+                    cal[i].push('<td><div class="dayInner">' + details.weekDays[j] + '</div></td>');
+                } else if (day > details.totalDays || j < start && i === 1) {
+                    cal[i].push('<td><div class="dayInner">&nbsp;</div></td>');
+                } else {
+                    cal[i].push(`<td class="${classes}" data-date="${dateString}" ${dayId ? 'data-id="' + dayId + '"' : ''}><div class="dayInner">${day++}${icons}</div></td>`);
+                }
+            }
+            cal[i].push('</tr>');
+        }
+        cal = cal.reduce(function (a, b) { return a.concat(b); }, []).join('');
+        document.querySelector('table').innerHTML += cal;
+        document.getElementById('month').textContent = details.months[d.getMonth()];
+        document.getElementById('year').textContent = d.getFullYear();
+
+        document.querySelectorAll('.day').forEach(function(day) {
+            day.addEventListener('mouseover', function() {
+                this.classList.add('hover');
+            });
+            day.addEventListener('mouseout', function() {
+                this.classList.remove('hover');
+            });
+            day.addEventListener('click', function(event) {
+                var targetClass = event.target.className;
+                var date = this.getAttribute('data-date');
+                var dayId = this.getAttribute('data-id');
+                if (targetClass.includes('edit-icon')) {
+                    sendTaskRequest('edit', dayId, date);
+                } else if (targetClass.includes('delete-icon')) {
+                    sendTaskRequest('delete', dayId, date);
+                } else if (targetClass.includes('add-icon')) {
+                    sendTaskRequest('add', null, date);
+                }
+            });
+        });
+    }
+
+    document.getElementById('left').addEventListener('click', function () {
+        document.querySelector('table').innerHTML = '';
+        currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1);
+        generateCalendar(currentDate);
     });
-     yearSelect.addEventListener('change', function() {
-     generateCalendar(this.value, monthSelect.value);
-     });
-     monthSelect.addEventListener('input', function() {
-     generateCalendar(yearSelect.value, this.value);
-     });
+
+    document.getElementById('right').addEventListener('click', function () {
+        document.querySelector('table').innerHTML = '';
+        currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1);
+        generateCalendar(currentDate);
+    });
+
+    generateCalendar(currentDate);
 });
-
-// Generate the current month's calendar on page load
-
-
-
-
