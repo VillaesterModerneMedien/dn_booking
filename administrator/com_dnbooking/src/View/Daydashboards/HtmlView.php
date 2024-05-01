20b88f0e15e2f8d4c64957b740769b976c5df487
Whykiki <info@whykiki.de>
2024-05-01 08:05:23 +0200

    Form hinzugefügt, damit XML choose_day funzt

--- administrator/com_dnbooking/src/View/Daydashboards/HtmlView.php
+++ administrator/com_dnbooking/src/View/Daydashboards/HtmlView.php
@@ -56,7 +56,10 @@ class HtmlView extends BaseHtmlView
 	public function display($tpl = null): void
 	{
 		$this->items         = $this->get('Items');
+		$this->pagination    = $this->get('Pagination');
 		$this->state         = $this->get('State');
+		$this->filterForm    = $this->get('FilterForm');
+		$this->activeFilters = $this->get('ActiveFilters');
 
 		$this->addToolbar();
 
@@ -72,11 +75,26 @@ class HtmlView extends BaseHtmlView
 	 */
 	private function addToolbar()
 	{
-		$date = date('d.m.Y');
 
+		$app = Factory::getApplication();
+		$input = $app->input;
+
+		$input = $app->input;
+		$date = $app->getUserState('com_dnbooking.daydashboards.currentDate', date('Y-m-d'));
+
+
+		if($date == '')
+		{
+			//$today = date('Y-m-d');
+			$date = date('d.m.Y');
+		}
+
+
+		// Get the toolbar object instance
+		$toolbar = Toolbar::getInstance('toolbar');
 		$headline = Text::sprintf('COM_DNBOOKING_HEADLINE_DAYDASHBOARDS', $date);
 		ToolbarHelper::title($headline, 'calendar');
-	
+
 		// Füge den "Print Daysheet" Button hinzu
 		ToolbarHelper::custom(
 			'daydashboards.printDaysheet',
@@ -86,6 +104,19 @@ class HtmlView extends BaseHtmlView
 			false
 		);
 
+		$dropdown = $toolbar->dropdownButton('status-group')
+			->text('JTOOLBAR_CHANGE_STATUS')
+			->toggleSplit(false)
+			->icon('icon-ellipsis-h')
+			->buttonClass('btn btn-action')
+			->listCheck(true);
+
+		$childBar = $dropdown->getChildToolbar();
+
+		$childBar->popupButton('chooseDay', 'COM_DNBOOKING_CHOOSEDAY_LABEL')
+			->selector('chooseDayModal')
+			->icon('icon-mail')
+			->listCheck(true);
 
 	}
 
