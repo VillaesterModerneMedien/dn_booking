85e7ecb74998a774fe14c7f1a8dd830c3d202ac4
Whykiki <info@whykiki.de>
2024-05-01 08:06:04 +0200

    Layout edit gefixed, analog details.php

--- administrator/com_dnbooking/tmpl/reservation/edit.php
+++ administrator/com_dnbooking/tmpl/reservation/edit.php
@@ -15,56 +15,132 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\User\UserHelper;
 
-$app = Factory::getApplication();
+$app   = Factory::getApplication();
 $input = $app->input;
 
 $wa = $this->document->getWebAssetManager();
-$wa->useScript('keepalive')
-   ->useScript('form.validate');
+$wa->useScript('joomla.dialog-autocreate');
+$wa->useStyle('com_dnbooking.reservation');
+$reservation     = $this->item;
+$customer = $this->customer;
 
-$layout  = 'edit';
+$layout  = 'details';
 $tmpl = $input->get('tmpl', '', 'cmd') === 'component' ? '&tmpl=component' : '';
+
+$reservationId = $reservation->id;
+if (empty($this->form->getValue('reservation_token')))
+{
+	$reservationToken = UserHelper::genRandomPassword(32);
+	$this->form->setValue('reservation_token', null, $reservationToken);
+}
 ?>
 
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const tabs = document.querySelectorAll('#myTabs .nav-link');
+        const tabPanes = document.querySelectorAll('.tab-pane');
+
+        tabs.forEach(tab => {
+            tab.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                tabs.forEach(t => t.classList.remove('active'));
+                tab.classList.add('active');
+
+                const targetPane = document.querySelector(tab.getAttribute('data-bs-target'));
+                tabPanes.forEach(pane => {
+                    pane.classList.remove('show');
+                    pane.classList.remove('active');
+                });
+
+                targetPane.classList.add('show');
+                targetPane.classList.add('active');
+            });
+        });
+    });
+
+</script>
+
 <div class="dnbooking dnbooking_reservation">
-	<form action="<?php echo Route::_('index.php?option=com_dnbooking&layout=' . $layout . $tmpl . '&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
-		<?php echo LayoutHelper::render('joomla.edit.title_alias', $this); ?>
+    <form action="<?php echo Route::_('index.php?option=com_dnbooking&layout=' . $layout . $tmpl . '&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm">
+        <div class="row">
 
-        <div class="main-card">
-            <?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', array('active' => 'details', 'recall' => true, 'breakpoint' => 768)); ?>
+            <div class="col-md-6">
+                <div class="card text-white bg-secondary mb-3">
+                    <div class="card-header"><h3><?= Text::_('COM_DNBOOKING_HEADING_RESERVATION_EDIT_HEADLINE'); ?></h3>
+                    </div>
+                    <div class="card-body">
+                        <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+                            <div class="container-fluid">
+                                <div class="collapse navbar-collapse" id="navbarNav">
+                                    <ul class="navbar-nav nav-tabs" id="myTabs" role="tablist">
+                                        <li class="nav-item" role="presentation">
+                                            <button class="nav-link active" id="tab1-tab" data-bs-toggle="tab"
+                                                    data-bs-target="#tab1" type="button" role="tab" aria-controls="tab1"
+                                                    aria-selected="true"><?= Text::_('COM_DNBOOKING_HEADING_RESERVATION_VIEW_HEADLINE'); ?></button>
+                                        </li>
 
-            <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'details', empty($this->item->id) ? Text::_('COM_DNBOOKING_NEW_RESERVATION') : Text::_('COM_DNBOOKING_EDIT_RESERVATION')); ?>
-            <div class="row">
-                <div class="col-lg-9">
-                    <?php echo $this->form->renderFieldset('reservationfieldset'); ?>
-                </div>
-                <div class="col-lg-3">
-                    <?php echo LayoutHelper::render('joomla.edit.global', $this); ?>
-                </div>
-            </div>
-            <?php echo HTMLHelper::_('uitab.endTab'); ?>
+                                        <li class="nav-item" role="presentation">
+                                            <button class="nav-link" id="tab2-tab" data-bs-toggle="tab"
+                                                    data-bs-target="#tab2" type="button" role="tab" aria-controls="tab2"
+                                                    aria-selected="false">
+												<?php if(!$reservationId): ?>
+													<?= Text::_('COM_DNBOOKING_NEW_CUSTOMER'); ?>
+												<?php else: ?>
+													<?= Text::_('COM_DNBOOKING_HEADING_CUSTOMER_EDIT_HEADLINE'); ?>
+												<?php endif; ?>
+                                            </button>
+                                        </li>
 
-            <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'publishing', Text::_('JGLOBAL_FIELDSET_PUBLISHING')); ?>
-            <div class="row">
-                <div class="col-lg-6">
-                    <fieldset id="fieldset-publishingdata" class="options-form">
-                        <legend><?php echo Text::_('JGLOBAL_FIELDSET_PUBLISHING'); ?></legend>
-                        <div>
-                            <?php echo LayoutHelper::render('joomla.edit.publishingdata', $this); ?>
+                                        <li class="nav-item" role="presentation">
+                                            <button class="nav-link" id="tab3-tab" data-bs-toggle="tab"
+                                                    data-bs-target="#tab3" type="button" role="tab" aria-controls="tab3"
+                                                    aria-selected="false"><?= Text::_('COM_DNBOOKING_HEADING_EXTRAS_EDIT_HEADLINE'); ?></button>
+                                        </li>
+
+                                        <li class="nav-item" role="presentation">
+                                            <button class="nav-link" id="tab4-tab" data-bs-toggle="tab"
+                                                    data-bs-target="#tab4" type="button" role="tab" aria-controls="tab4"
+                                                    aria-selected="false"><?= Text::_('COM_DNBOOKING_HEADING_ADDINFOS2_HEADLINE'); ?></button>
+                                        </li>
+
+                                        <li class="nav-item" role="presentation">
+                                            <button class="nav-link" id="tab5-tab" data-bs-toggle="tab"
+                                                    data-bs-target="#tab5" type="button" role="tab" aria-controls="tab5"
+                                                    aria-selected="false"><?= Text::_('COM_DNBOOKING_HEADING_NOTES_HEADLINE'); ?></button>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+
+                        <div class="tab-content" id="meineTabsContent">
+
+							<?php echo LayoutHelper::render('reservation.reservation_tab', $this); ?>
+							<?php echo LayoutHelper::render('reservation.customer_tab', $customer); ?>
+							<?php echo LayoutHelper::render('reservation.extras_tab', $this); ?>
+							<?php echo LayoutHelper::render('reservation.addinfos2_tab', $this); ?>
+							<?php echo LayoutHelper::render('reservation.notes_tab', $this); ?>
+
                         </div>
-                    </fieldset>
-                </div>
-                <div class="col-lg-6">
-                    <!-- second col -->
+                    </div>
                 </div>
             </div>
-            <?php echo HTMLHelper::_('uitab.endTab'); ?>
 
-            <?php echo HTMLHelper::_('uitab.endTabSet'); ?>
+            <div class="col-md-6">
+               Reservierung anlegen
+            </div>
+
         </div>
 
+        <div hidden>
+
+			<?php echo $this->form->renderFieldset('mybasic'); ?>
+
+        </div>
 		<?php echo HTMLHelper::_('form.token'); ?>
-		<input type="hidden" name="task" value="">
-	</form>
+        <input type="hidden" name="task" value="">
+    </form>
 </div>
