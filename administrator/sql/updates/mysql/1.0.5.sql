dc305cdeed1ad28c8500858a3dbd360ab21ed870
Mario Hewera <mh@villaester.de>
2025-10-13 12:53:25 +0200

    Removes obsolete update schema definitions
    Removes the now unused update schema definitions from the XML manifest files.
    
    These definitions pointed to SQL update files that have been removed,
    simplifying the component structure.

--- administrator/sql/updates/mysql/1.0.5.sql
+++
@@ -1,8 +0,0 @@
-# Conversion to utf8mb4
-
---
--- ALTER TABLE `#__dnbooking_ ...`
---
--- Drop specific columns if they exist
-ALTER TABLE `#__dnbooking_reservations`
-    MODIFY COLUMN `extra_ids` LONGTEXT;
